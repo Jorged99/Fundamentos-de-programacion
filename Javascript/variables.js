29ae976e9a0b9d987302ff2045d966375b0c9582
Jorged99 <jorgeald724@gmail.com>
2026-01-20 21:06:42 -0600

    Delete Javascript directory
    No needed

--- Javascript/variables.js
+++
@@ -1,9 +0,0 @@
-// Usando var (no recomendado en ES6+)
-var nombre = "Juan";
-console.log(nombre); // Output: Juan
-
-// Usando let (permite reasignación)
-let edad = 25;
-console.log(edad); // Output: 25
-edad = 26;
-console.log(edad); // Output: 26 
