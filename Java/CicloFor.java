a49200d2a61c7a8b6876aff50217682aa0d2c5cb
Jorged99 <jorgeald724@gmail.com>
2026-01-21 07:50:53 -0600

    Add CicloFor.java with a for loop example

---
+++ Java/CicloFor.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Estructura: (inicio; condición; incremento)
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Número en Java: " + i);
+        }
+    }
+}
